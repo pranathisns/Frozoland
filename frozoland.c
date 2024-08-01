682978372cb008c1e08ce6d88e2bf6ccf53bf53f
pranathisns <pranathisns25@gmail.com>
2024-08-01 12:10:10 +0530

    second commit

--- frozoland.c
+++ frozoland.c
@@ -325,9 +325,9 @@ void readStockFromFile()
 {
     FILE *stockFile = fopen("stock.txt", "r");
     if (stockFile != NULL) {
-        fscanf(stockFile, "%d %d %d %d\n", &strawberrystock, &blueberrystock, &vanillastock, &chocolatestock);
-        fscanf(stockFile, "%lf %lf %lf %lf\n", &applestock, &Blueberrystock, &Strawberrystock, &Mangostock);
-        fscanf(stockFile, "%lf %lf %lf %lf\n", &chocolatesaucestock, &sprinklesstock, &caramelsaucestock, &whippedcreamstock);
+        fscanf(stockFile, "strawberrystock:%d blueberrystock:%d vanillastock:%d chocolatestock:%d\n", &strawberrystock, &blueberrystock, &vanillastock, &chocolatestock);
+        fscanf(stockFile, "applestock:%lf Blueberrystock:%lf Strawberrystock:%lf Mangostock:%lf\n", &applestock, &Blueberrystock, &Strawberrystock, &Mangostock);
+        fscanf(stockFile, "chocolatesaucestock:%lf sprinklesstock:%lf caramelsaucestock:%lf whippedcreamstock:%lf\n", &chocolatesaucestock, &sprinklesstock, &caramelsaucestock, &whippedcreamstock);
         fclose(stockFile);
     } else {
         printf("Error: Could not open stock file for reading.\n");
